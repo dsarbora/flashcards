fdae67e97f4046aa5a7ca99d950c906387d228c5
Dave Sarbora and Jared Hanson <dave+jared@>
2019-01-15 10:52:49 -0800

    add html skeleton for flashcards

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function(){
+  $("button#next").click(function() {
+    $("next").addClass();
+  });
+
+  $("button#back").click(function() {
+    $("body").removeClass();
+    $("body").addClass("");
+  });
+});
+
+
+
+
+
+$("#x").slideToggle();
+    $("#y").slideToggle();
+
+
+var counter = 0;
