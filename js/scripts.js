3f4f7bf35c9efb8310fc5d46a52b2998c1bd18e1
Dave Sarbora <dsarbora@gmail.com>
2019-01-15 22:39:49 -0800

    fix the problem of adding an event listener each time the card was displayed

--- js/scripts.js
+++ js/scripts.js
@@ -1,245 +1,238 @@
-// $(function(){
-//   $("button#next").click(function() {
-//     $("next").addClass();
-//   });
-//
-//   $("button#back").click(function() {
-//     $("body").removeClass();
-//     $("body").addClass("");
-//   });
-// });
+
 $(function(){
   $(".panel").hide();
   $(".panel.JS").show();
   $(".panel.JS").click(function(){
     $("p.JS").toggle();
   });
+  $(".panel.operators").click(function(){
+    $("p.operators").toggle();
+  });
+  $(".panel.variables").click(function(){
+    $("p.variables").toggle();
+  });
+  $(".panel.variableNamingConventions").click(function(){
+    $("p.variableNamingConventions").toggle();
+  });
+  $(".panel.functions").click(function(){
+    $("p.functions").toggle();
+  });
+  $(".panel.methods").click(function(){
+    $("p.methods").toggle();
+  });
+  $(".panel.arguments").click(function(){
+    $("p.arguments").toggle();
+  });
+  $(".panel.parameters").click(function(){
+    $("p.parameters").toggle();
+  });
+  $(".panel.return").click(function(){
+    $("p.return").toggle();
+  });
+  $(".panel.chainingMethods").click(function(){
+    $("p.chainingMethods").toggle();
+  });
+  $(".panel.strings").click(function(){
+    $("p.strings").toggle();
+  });
+  $(".panel.booleans").click(function(){
+    $("p.booleans").toggle();
+  });
+  $(".panel.undefined").click(function(){
+    $("p.undefined").toggle();
+  });
+  $(".panel.NaN").click(function(){
+    $("p.NaN").toggle();
+  });
+  $(".panel.escape").click(function(){
+    $("p.escape").toggle();
+  });
+  $(".panel.alert").click(function(){
+    $("p.alert").toggle();
+  });
+  $(".panel.comments").click(function(){
+    $("p.comments").toggle();
+  });
+  $(".panel.jQuery").click(function(){
+    $("p.jQuery").toggle();
+  });
+  $(".panel.attributes").click(function(){
+    $("p.attributes").toggle();
+  });
+
+
   var counter = 0;
   var text;
   $("#next").click(function() {
     counter++;
-    console.log(counter)
-    if (counter >= 18) {
-      counter = 0;
-      $(".panel").hide();
-       $(".panel.JS").show();
-       $(".panel.JS").click(function(){
-         $("p.JS").toggle();
-     })};
     if (counter === 0){
       $(".panel").hide();
       $(".panel.JS").show();
-      $(".panel.JS").click(function(){
-        $("p.JS").toggle();
-      });
     }
     else if (counter === 1) {
       $(".panel").hide();
       $(".panel.operators").show();
-      $(".panel.operators").click(function(){
-        $("p.operators").toggle();
-      });
     }
     else if (counter === 2){
       $(".panel").hide();
       $(".panel.variables").show();
-      $(".panel.variables").click(function(){
-        $("p.variables").toggle();
-      });
     }
     else if (counter === 3) {
       $(".panel").hide();
       $(".panel.variableNamingConventions").show();
-      $(".panel.variableNamingConventions").click(function(){
-        $("p.variableNamingConventions").toggle();
-      })
     }
     else if (counter === 4) {
       $(".panel").hide();
       $(".panel.functions").show();
-      $(".panel.functions").click(function(){
-        $("p.functions").toggle();
-      })
     }
     else if (counter === 5) {
       $(".panel").hide();
       $(".panel.methods").show();
-      $(".panel.methods").click(function(){
-        $("p.methods").toggle();
-      })
     }
     else if (counter === 6) {
       $(".panel").hide();
       $(".panel.arguments").show();
-      $(".panel.arguments").click(function(){
-        $("p.arguments").toggle();
-      })
     }
     else if (counter === 7) {
       $(".panel").hide();
       $(".panel.parameters").show();
-      $(".panel.parameters").click(function(){
-        $("p.parameters").toggle();
-      })
     }
     else if (counter === 8) {
       $(".panel").hide();
       $(".panel.return").show();
-      $(".panel.return").click(function(){
-        $("p.return").toggle();
-      })
     }
     else if (counter === 9) {
       $(".panel").hide();
       $(".panel.chainingMethods").show();
-      $(".panel.chainingMethods").click(function(){
-        $("p.chainingMethods").toggle();
-      })
     }
     else if (counter === 10) {
       $(".panel").hide();
       $(".panel.strings").show();
-      $(".panel.strings").click(function(){
-        $("p.strings").toggle();
-      })
     }
     else if (counter === 11) {
       $(".panel").hide();
       $(".panel.booleans").show();
-      $(".panel.booleans").click(function(){
-        $("p.booleans").toggle();
-      })
     }
     else if (counter === 12) {
       $(".panel").hide();
       $(".panel.undefined").show();
-      $(".panel.undefined").click(function(){
-        $("p.undefined").toggle();
-      })
     }
     else if (counter === 13) {
       $(".panel").hide();
       $(".panel.NaN").show();
-      $(".panel.NaN").click(function(){
-        $("p.NaN").toggle();
-      })
     }
     else if (counter === 14) {
       $(".panel").hide();
       $(".panel.escape").show();
-      $(".panel.escape").click(function(){
-        $("p.escape").toggle();
-      })
     }
     else if (counter === 15) {
       $(".panel").hide();
       $(".panel.alert").show();
-      $(".panel.alert").click(function(){
-        $("p.alert").toggle();
-      })
     }
     else if (counter === 16) {
       $(".panel").hide();
       $(".panel.comments").show();
-      $(".panel.comments").click(function(){
-        $("p.comments").toggle();
-      })
     }
     else if (counter === 17) {
       $(".panel").hide();
       $(".panel.jQuery").show();
-      $(".panel.jQuery").click(function(){
-        $("p.jQuery").toggle();
-      })
     }
     else if (counter === 18) {
       $(".panel").hide();
       $(".panel.attributes").show();
-      $(".panel.attributes").click(function(){
-        $("p.attributes").toggle();
-      })
     }
 
-    // else{
-    //   counter = 0;
-    //   $(".panel").hide();
-    //   $(".panel.JS").show();
-    //   $(".panel.JS").click(function(){
-    //     $("p.JS").toggle();
-    //   })
-    // };
+     else{
+       counter = 0;
+       $(".panel").hide();
+       $(".panel.JS").show();
+     };
   });
 
     $("#back").click(function(){
       counter--;
 
       if (counter === 0){
-        text = "0";
+        $(".panel").hide();
+        $(".panel.JS").show();
       }
       else if (counter === 1) {
-        text = "1";
+        $(".panel").hide();
+        $(".panel.operators").show();
       }
       else if (counter === 2){
-        text = "2"
+        $(".panel").hide();
+        $(".panel.variables").show();
       }
       else if (counter === 3) {
-        text = "3";
+        $(".panel").hide();
+        $(".panel.variableNamingConventions").show();
       }
       else if (counter === 4) {
-        text = "4";
+        $(".panel").hide();
+        $(".panel.functions").show();
       }
       else if (counter === 5) {
-        text = "5";
+        $(".panel").hide();
+        $(".panel.methods").show();
       }
       else if (counter === 6) {
-        text = "6";
+        $(".panel").hide();
+        $(".panel.arguments").show();
       }
       else if (counter === 7) {
-        text = "7";
+        $(".panel").hide();
+        $(".panel.parameters").show();
       }
       else if (counter === 8) {
-        text = "8";
+        $(".panel").hide();
+        $(".panel.return").show();
       }
       else if (counter === 9) {
-        text = "9";
+        $(".panel").hide();
+        $(".panel.chainingMethods").show();
       }
       else if (counter === 10) {
-        text = "10";
+        $(".panel").hide();
+        $(".panel.strings").show();
       }
       else if (counter === 11) {
-        text = "11";
+        $(".panel").hide();
+        $(".panel.booleans").show();
       }
       else if (counter === 12) {
-        text = "12";
+        $(".panel").hide();
+        $(".panel.undefined").show();
       }
       else if (counter === 13) {
-        text = "13";
+        $(".panel").hide();
+        $(".panel.NaN").show();
       }
       else if (counter === 14) {
-        text = "14";
+        $(".panel").hide();
+        $(".panel.escape").show();
       }
       else if (counter === 15) {
-        text = "15";
+        $(".panel").hide();
+        $(".panel.alert").show();
       }
       else if (counter === 16) {
-        text = "16";
+        $(".panel").hide();
+        $(".panel.comments").show();
       }
       else if (counter === 17) {
-        text = "17";
+        $(".panel").hide();
+        $(".panel.jQuery").show();
       }
       else if (counter === 18) {
-        text = "18";
+        $(".panel").hide();
+        $(".panel.attributes").show();
       }
-      else {
-        counter = 18;
-        text = "18"
-      }
-    });
-});
 
-$(function() {
-  $(".clickable").click(function() {
-    $("#initial-hide").slideToggle();
-    $("#initial-show").slideToggle();
-  });
+       else{
+         counter = 18;
+         $(".panel").hide();
+         $(".panel.attributes").show();
+       };
+    });
 });
